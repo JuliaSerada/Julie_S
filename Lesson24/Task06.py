1063ac34b589af8d255bc0a0ba1bfbd18224efa6
JuliaSerada <julia.serada@gmail.com>
2023-04-18 21:14:21 +0300

    Del script

---
+++ Lesson24/Task06.py
@@ -0,0 +1,6 @@
+students = {"Alex": 10, "Peter": 9, "Victor": 7, "Anna": 8, "Kate": 8, "Vova": 10, "Nikita": 8}
+
+print("Before:", students)
+students.pop("Victor")
+# del students["Victor"]
+print("After:", students)
