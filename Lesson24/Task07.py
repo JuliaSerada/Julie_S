1fbeccc3f909d35b11bf48f2651b488a4d3c806f
JuliaSerada <julia.serada@gmail.com>
2023-04-18 21:43:30 +0300

    New script

---
+++ Lesson24/Task07.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+
+pprint(dir(dict))
+
+# students = {"Alex": 10, "Peter": 9, "Victor": 7, "Anna": 8, "Kate": 8, "Vova": 10, "Nikita": 8}
+
+# print("Alex" in students)
+# print("Alex" in students.keys())
+# print("Alex" not in students)
+# print(5 in students.values())
+# print(10 not in students.values())
---
+++ Lesson24/Task08.py
@@ -0,0 +1,7 @@
+empty_dict1 = {}
+empty_dict2 = dict()
+# d1 = dict([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)])
+# print(d1)
+
+d1 = dict.fromkeys("abcde", 0)
+print(d1)
