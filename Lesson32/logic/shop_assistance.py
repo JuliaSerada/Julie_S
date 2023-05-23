abf89a4a95e502dc31a43095fe68d9e2e0c31a76
JuliaSerada <julia.serada@gmail.com>
2023-05-23 21:32:31 +0300

    Lesson32 - Add staticmethod

--- Lesson32/logic/shop_assistance.py
+++ Lesson32/logic/shop_assistance.py
@@ -1,4 +1,25 @@
-class ShopAssistance:
+from container.basket import Basket
+from entity.milk import Milk
+from entity.bread import Bread
+from entity.orange import Orange
 
+
+class ShopAssistance:
     @staticmethod
-    def
+    def calculate_total_price(basket):
+        if isinstance(basket, Basket) and basket.size != 0:
+            total = 0
+            for i in range(basket.size):
+                product = basket.get_product(i)
+
+                if isinstance(product, Milk):
+                    total += product.money
+
+                if isinstance(product, Bread):
+                    total += product.price
+
+                if isinstance(product, Orange):
+                    total += product.cost
+
+            return total
+
--- Lesson32/main.py
+++ Lesson32/main.py
@@ -2,6 +2,7 @@ from entity.milk import Milk
 from entity.bread import Bread
 from entity.orange import Orange
 from container.basket import Basket
+from logic.shop_assistance import ShopAssistance
 
 
 # br = Bread()
@@ -15,8 +16,8 @@ from container.basket import Basket
 def main():
     basket = Basket()
 
-    br = Bread()
-    o = Orange()
+    br = Bread("white", "second", 3.0)
+    o = Orange(200, 3000, 1.5)
     m = Milk(1, 4.2, 5.5)
 
     basket.add(br)
@@ -25,6 +26,9 @@ def main():
     print(f"size = {basket.size}")
 
     print(basket)
+
+    total = ShopAssistance.calculate_total_price(basket)
+    print(f"Total price is {total}")
 
 
 if __name__ == "__main__":
